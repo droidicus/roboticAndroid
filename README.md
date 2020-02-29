8ea9a911b4baa0f37fe1864b72fb9d034b9b6010
droidicus <droidicus@dr.com>
2020-02-28 19:40:59 -0500

    lots more analysis and other stuff

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # roboticAndroid
 Discord bot for data analytics
+
+# Get a copy of the project
+* Clone the repository
+* Create a bot account with discord, copy the token that is generated during this process
+* Open the `token` file in the root directory with a text editor
+* Paste the token copied above and ensure it is on a single line
+* Save the `token` file
+
+# Set up environment on a Window 10 system
+## Install Miniconda
+* See instructions here: https://docs.conda.io/en/latest/miniconda.html
+## Base environment setup
+```
+conda create -n nbstripout
+conda activate nbstripout
+conda install -c conda-forge nbstripout
+```
+* Navigate to the cloned repo
+```
+nbstripout --install
+conda deactivate nbstripout
+```
+## Jupyter Lab environment
+```
+conda create -n jupyterlab python jupyterlab ipympl widgetsnbextension nodejs ipywidgets
+conda activate jupyterlab
+set NODE_OPTIONS=--max-old-space-size=4096
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+jupyter labextension install jupyterlab-plotly --no-build
+jupyter labextension install plotlywidget --no-build
+jupyter labextension install jupyter-matplotlib --no-build
+jupyter lab build
+set NODE_OPTIONS=
+conda deactivate
+```
+## Discord conda environment
+```
+conda create -n discord python matplotlib plotly numpy pandas tqdm ipykernel fastparquet python-snappy pylint scipy ipympl
+conda activate discord
+pip install discord.py
+python -m ipykernel install --user --name discord --display-name "Python (discord)"
+conda deactivate
+```
+## Tensorflow environment
+* Install `Microsoft Visual C++ Redistributable for Visual Studio 2015, 2017 and 2019` from here: https://support.microsoft.com/help/2977003/the-latest-supported-visual-c-downloads
+```
+conda create -n tensorflow python tensorflow-gpu tensorflow-hub tensorflow-datasets matplotlib plotly pandas tqdm ipykernel fastparquet python-snappy pylint ipympl pytables h5py
+conda activate tensorflow
+python -m ipykernel install --user --name tensorflow --display-name "Python (tensorflow)"
+conda deactivate
+```
+
+```
+conda create -n tensorflow python matplotlib plotly pandas tqdm ipykernel fastparquet python-snappy pylint ipympl pytables h5py
+conda activate tensorflow
+pip install tensorflow tensorflow-hub tensorflow-datasets
+python -m ipykernel install --user --name tensorflow --display-name "Python (tensorflow)"
+conda deactivate
+```
+
+# How to run
+## To run the bot to gather data from the server history
+* Open the CMD prompt.
+* Navigate to the repository clone/copy
+```
+conda activate discord
+python discord.py
+conda deactivate
+```
+## How to launch jupyter lab for data analysis
+* Open the CMD prompt
+* Navigate to the repository clone/copy
+```
+conda activate jupyterlab
+jupyter lab
+```
+* Open the Jupyter Lab environment in the browser of your choice
+* Open `main.ipynb`
+* Ensure that the `discord` python environment is selected
+* Run the cells of interest
--- main.py
+++ main.py
@@ -3,26 +3,65 @@ import datetime
 import json
 import sys
 import time
+import os
+from pprint import pprint
 
 import discord
 import pandas as pd
 from tqdm import tqdm
 
+################################################################
+# TODO list:
+# Add incremental mode to update existing file with new messages
+# Add command line options
+################################################################
+
 # Constants and such
 NOW = datetime.datetime.utcnow()
-# LOOKBACK_AFTER = None
-# LOOKBACK_AFTER = NOW - datetime.timedelta(days=365)
+# LOOKBACK_AFTER = None # ALL history! (Warning this takes a while)
+# LOOKBACK_AFTER = NOW - datetime.timedelta(days=365) (Warning this takes a while)
 # LOOKBACK_AFTER = NOW - datetime.timedelta(days=30)
 # LOOKBACK_AFTER = NOW - datetime.timedelta(days=7)
-LOOKBACK_AFTER = NOW - datetime.timedelta(days=1)
-# LOOKBACK_AFTER = NOW - datetime.timedelta(hours=1)
-FILE_NAME = 'test'
-OUTPUT_DATA_FILE = f'{FILE_NAME}_data.parquet'
-OUTPUT_METADATA_FILE = f'{FILE_NAME}_meta.parquet'
-FILE_VERSION = 'pre-0.0.0'
+# LOOKBACK_AFTER = NOW - datetime.timedelta(days=1)
+LOOKBACK_AFTER = NOW - datetime.timedelta(hours=1)
+OUTPUT_FILE_NAME = 'test'
+OUTPUT_FILE_DIR = 'data'
+OUTPUT_DATA_FILE = os.path.join(OUTPUT_FILE_DIR, f'{OUTPUT_FILE_NAME}_data.parquet')
+OUTPUT_METADATA_FILE = os.path.join(OUTPUT_FILE_DIR, f'{OUTPUT_FILE_NAME}_meta.parquet')
+FILE_VERSION = 'alpha0-0.0.0'
+
+DO_HISTORY_ONE_SHOT = True # 'True' for history parsing, 'False' for real-time parsing
+
 
 # Where the roboticAndroid lives
 client = discord.Client()
+
+# repack the interesting parts of the message
+def _message_to_dict(msg):
+    return {
+        'attachments_len': len(msg.attachments),
+        'attachments_ids': [i.id for i in msg.attachments],
+        'attachments_sizes': [i.size for i in msg.attachments],
+        'attachments_filenames': [i.filename for i in msg.attachments],
+        'attachments_urls': [i.url for i in msg.attachments],
+        'author': str(msg.author),
+        'author_name': msg.author.name,
+        'author_displayname': msg.author.nick if type(msg.author) == discord.member.Member else '',
+        'author_id': msg.author.id,
+        'content': msg.content,
+        'channel_name': msg.channel.name,
+        'channel_id': msg.channel.id,
+        'creation_datetime': msg.created_at,
+        'guild_name': msg.guild.name,
+        'guild_id': msg.guild.id,
+        'reactions_len': len(msg.reactions),
+        'reactions_emojis': [str(i.emoji) for i in msg.reactions],
+        'reactions_counts': [i.count for i in msg.reactions],
+        'type': str(msg.type),
+        # This slows down history parsing a LOT due to the await'ed call to get all of the users,
+        # up to an order of magnitude slowdown on heavily-reactioned channels. May not be needed?
+        # 'reactions_userids': [[k.id for k in j] for j in [await i.users().flatten() for i in msg.reactions]],
+    }
 
 @client.event
 async def on_disconnect():
@@ -30,26 +69,10 @@ async def on_disconnect():
 
 @client.event
 async def on_ready():
-    def message_to_dict(msg):
-        return {
-            'author': str(msg.author),
-            'author_name': msg.author.name,
-            'author_displayname': msg.author.nick if msg.author == discord.member.Member else '',
-            'author_id': msg.author.id,
-            'content': msg.content,
-            'chanel': str(msg.channel),
-            'channel_name': msg.channel.name,
-            'channel_id': msg.channel.id,
-            # 'mentions': msg.mentions,
-            'guild': str(msg.guild),
-            'guild_name': msg.guild.name,
-            'guild_id': msg.guild.id,
-            # 'reactions': msg.reactions,
-            'type': str(msg.type),
-            'creation_datetime': msg.created_at,
-        }
-
     print(f'We have logged in as {client}')
+
+    if not DO_HISTORY_ONE_SHOT:
+        return
 
     # Start gathering all of the data
     total_messages = 0
@@ -72,7 +95,8 @@ async def on_ready():
                         total_messages += 1
 
                         # Add dict of data to the list
-                        channel_elem_dict_list.append(message_to_dict(elem))
+                        elem_dict = _message_to_dict(elem)
+                        channel_elem_dict_list.append(elem_dict)
 
                     # Channel complete, append data to dataframe
                     data = data.append(channel_elem_dict_list, ignore_index=True)
@@ -85,33 +109,61 @@ async def on_ready():
     print('*******************************************************************')
     print(f'Processing took: {then-NOW}')
     print(f'Total messages parsed: {total_messages}')
-    print(f'Parsing speed: {total_messages/(then-NOW).total_seconds():.2f} msg/sec')
+    print(f'Parsing speed avg: {total_messages/(then-NOW).total_seconds():.2f} msg/sec')
 
     # Save data and metadata to parquet files
+    os.makedirs('data', exist_ok=True)
     print('')
     print(f'Saving data to - {OUTPUT_DATA_FILE}')
-    data.to_parquet(OUTPUT_DATA_FILE)
+    data.to_parquet(OUTPUT_DATA_FILE, compression='gzip')
     print(f'Saving metadata to - {OUTPUT_METADATA_FILE}')
     pd.DataFrame([{
         'file_version': FILE_VERSION,
         'start_datetime': LOOKBACK_AFTER,
         'end_datetime': NOW,
         'collection_datetime': then,
-        }]).to_parquet(OUTPUT_METADATA_FILE)
+        }]).to_parquet(OUTPUT_METADATA_FILE, compression=None)
 
     # Close the bot when we are complete, this exits the bot
     print('')
     print('DONE!')
-    await client.close()
+    await shutdown(client.loop)
+
+# This method is called for each new message that the bot can see
+@client.event
+async def on_message(message):
+    if DO_HISTORY_ONE_SHOT:
+        return
+
+    message_dict = _message_to_dict(message)
+
+    print('')
+    print(message)
+    pprint(message_dict, indent=4)
+
+# Hand exceptions
+def handle_exception(loop, context):
+    # context["message"] will always be there; but context["exception"] may not
+    msg = context.get("exception", context["message"])
+    print('')
+    print(f"Caught exception: {msg}")
+    print("Shutting down...")
+    asyncio.create_task(shutdown(loop))
+
+# Shutdown the application
+async def shutdown(loop, signal=None):
+    """Cleanup tasks tied to the service's shutdown."""
+    if signal:
+        print(f"Received exit signal {signal.name}...")
+    # <-- snip -->
     print('Closing loop, bot shutting down.')
+    await client.close()
 
-# # This method is called for each new message that the bot can see
-# @client.event
-# async def on_message(message):
-#     print(message)
-#     # print(f'{message.channel} - {message.author}: {message.content}')
-
+# Let's get started
 if __name__ == "__main__":
+    # Setup exception handling
+    client.loop.set_exception_handler(handle_exception) # TODO: This doesn't seem to be working...
+
     # Read the secret token from disk, change this file to contain your token
     with open('token', 'r') as f:
         token = f.readline()
