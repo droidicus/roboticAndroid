d6b048f0c25da04a62b0d17612b761eb56772838
droidicus <droidicus@dr.com>
2020-03-01 12:18:21 -0500

    renaming main files

--- test.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open('token', 'r') as f:
-        token = f.readline()
-        print('Token is: {}'.format(token))
-
-if __name__ == "__main__":
-    main()
-    print('all done')
